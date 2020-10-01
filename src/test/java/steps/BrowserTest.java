17a58425d0a079a898dd394564f288e4c40a410d
581486 <581486@PC452802.cts.com>
2020-10-01 23:26:55 +0530

    updated

--- src/test/java/steps/BrowserTest.java
+++ src/test/java/steps/BrowserTest.java
@@ -1,12 +1,18 @@
 package steps;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.nio.file.Paths;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.*;
 import io.cucumber.java.en.*;
+
 
 public class BrowserTest {
 	private WebDriver driver;  
@@ -33,12 +39,20 @@ public void launch_the_application() {
 }
 
 @When("user perfroms actions")
-public void user_perfroms_actions() {
+public void user_perfroms_actions() throws AWTException, InterruptedException {
+	driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("report portal");
+	driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]")).click();
+	
   
 }
 
+
 @Then("validate the home page")
-public void validate_the_home_page() {
+public void validate_the_home_page() throws InterruptedException {
+	Thread.sleep(5000); 
+	String actual=driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div[1]/a/div/cite")).getText();
+	System.out.println("Text: " + actual);
+	Assert.assertEquals("reportportal.io", actual);
     
 }
 
